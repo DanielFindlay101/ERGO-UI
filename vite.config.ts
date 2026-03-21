851579d225f042cde27299c8cfaf93a85f1b76d4
Daniel Findlay <danielfindlay95@gmail.com>
2026-03-21 18:30:44 +0000

    updates to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      exclude: ['src/**/*.stories.tsx', 'src/main.tsx', 'src/App.tsx'],
+      outDir: 'dist/types',
+    }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'ErgoUI',
+      fileName: (format) => `ergo-ui.${format === 'es' ? 'js' : 'cjs'}`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    cssCodeSplit: false,
+  },
 })
